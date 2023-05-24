bbd0e63ced4f57895780affdd5722cb6ca8e6f41
Yuliia Ukrainets <ulaukrainec@gmail.com>
2023-05-24 15:40:32 +0200

    Added a link to my Github

--- src/App.js
+++ src/App.js
@@ -3,7 +3,19 @@ import React from "react";
 function App() {
   return (
     <div className="App">
-      <p>hsbb</p>
+      <h1>Weather App</h1>
+      <footer>
+        <p>
+          This project was created by Yuliia Ukrainets and it is<span> </span>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/Julyuk/react-weather-app"
+          >
+            open-sourced on Github
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }

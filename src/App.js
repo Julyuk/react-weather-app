0b6309b4445d51705bfa21503905087a14768da6
Yuliia Ukrainets <ulaukrainec@gmail.com>
2023-05-26 23:55:18 +0200

    Added basic styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import React from "react";
 import Weather from "./Weather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <h1 className="weather-app-h1">Weather App</h1>
       <Weather />
       <footer>
         <p>
--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,55 @@
-import "./App.css";
 import React from "react";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Weather.css";
+
 function Weather() {
-  return <p>Hello</p>;
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>New York</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Sunny</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6 together">
+          <img
+            src="https://ssl.gstatic.com/onebox/weather/64/sunny.png"
+            alt="Sunny"
+          />
+          <div className="Temp">
+            <span className="temperature">6</span>
+            <span className="unit ">°C</span>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 0%</li>
+            <li>Humidity: 27%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Weather;

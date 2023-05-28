35a5e7834607c22438d448031aab070690d1e534
Yuliia Ukrainets <ulaukrainec@gmail.com>
2023-05-28 14:33:42 +0200

    Fixed bugs

--- src/Weather.js
+++ src/Weather.js
@@ -34,60 +34,39 @@ export default function Weather(props) {
     search();
   }
   let form = (
-    <div>
-      <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-                onChange={updateCity}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
+    <div className="Weather">
+      <form onSubmit={handleSubmit}>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+              onChange={updateCity}
+            />
           </div>
-        </form>
-        <WeatherInfo data={weatherData} />
-      </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
+          </div>
+        </div>
+      </form>
     </div>
   );
   if (weatherData.ready) {
+    console.log(weatherData);
     return (
       <div className="Weather">
-        <form onSubmit={handleSubmit}>
-          <div className="row">
-            <div className="col-9">
-              <input
-                type="search"
-                placeholder="Enter a city..."
-                className="form-control"
-                autoFocus="on"
-                onChange={updateCity}
-              />
-            </div>
-            <div className="col-3">
-              <input
-                type="submit"
-                value="Search"
-                className="btn btn-primary w-100"
-              />
-            </div>
-          </div>
-        </form>
+        {form}
         <WeatherInfo data={weatherData} />
       </div>
     );
   } else {
     search();
-    return form;
+    return <div className="Weather">{form}</div>;
   }
 }
